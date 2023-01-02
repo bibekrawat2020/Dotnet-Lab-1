11de32ce0c9da58a2706a84a23e9ea4dfa8785e6
Bibek Rawat <bibekrawat14@gmail.com>
2023-01-02 15:07:59 +0545

    Reformatted the previous code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿string name = "steve";
-if (name == "bob") Console.WriteLine("Found Bob");
-else if (name == "steve") Console.WriteLine("Found Steve");
-else Console.WriteLine("Found Chuck");
+if (name == "bob") 
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else 
+    Console.WriteLine("Found Chuck");
